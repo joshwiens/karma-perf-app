1ae4df5ac8424f911c2b4600daae6aed121a2c5c
Joshua Wiens <joshuaw@easymetrics.com>
2017-05-02 17:55:41 -0500

    feat: add app module imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,27 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { AppRoutingModule } from './app-routing.module';
+import { AllInlineModule } from './all-inline/all-inline.module';
+import { InlineStyleModule } from './inline-style/inline-style.module';
+import { InlineTemplateModule } from './inline-template/inline-template.module';
+import { StandardComponentModule } from './standard-component/standard-component.module';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AppRoutingModule,
+    AllInlineModule,
+    InlineStyleModule,
+    InlineTemplateModule,
+    StandardComponentModule
   ],
   providers: [],
   bootstrap: [AppComponent]
