f11c9109d31a5b93938f5de2f1a37b15e94deaf8
Joshua Wiens <joshuaw@easymetrics.com>
2017-05-05 00:17:47 -0500

    refactor: use single lazy module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,19 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { AllInlineComponent } from './all-inline/all-inline.component';
+import { InlineStyleComponent } from './inline-style/inline-style.component';
+import { InlineTemplateComponent } from './inline-template/inline-template.component';
+import { StandardComponentComponent } from './standard-component/standard-component.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent, pathMatch: 'full' },
-  { path: 'all-inline', loadChildren: './all-inline/all-inline.module#AllInlineModule' },
-  { path: 'inline-style', loadChildren: './inline-style/inline-style.module#InlineStyleModule' },
-  { path: 'inline-template', loadChildren: './inline-template/inline-template.module#InlineTemplateModule' },
+  { path: 'all-inline', component: AllInlineComponent, pathMatch: 'full' },
+  { path: 'inline-style', component: InlineStyleComponent, pathMatch: 'full' },
+  { path: 'inline-template', component: InlineTemplateComponent, pathMatch: 'full' },
+  { path: 'standard-component', component: StandardComponentComponent, pathMatch: 'full' },
   { path: 'lazy-module', loadChildren: './lazy-module/lazy-module.module#LazyModuleModule' },
-  { path: 'standard-component', loadChildren: './standard-component/standard-component.module#StandardComponentModule' },
 ];
 
 @NgModule({
