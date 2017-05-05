35be0dd246072c1f1348e2311fefff920b03d755
Joshua Wiens <joshuaw@easymetrics.com>
2017-05-05 02:18:30 -0500

    test(inline style): Adds dynamic css test

--- src/app/inline-style/is-testcomp-c/is-testcomp-c.component.ts
+++ src/app/inline-style/is-testcomp-c/is-testcomp-c.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'kwp-is-testcomp-c',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class IsTestcompCComponent implements OnInit {
+  @Input() isActive = false;
 
   constructor() { }
 
