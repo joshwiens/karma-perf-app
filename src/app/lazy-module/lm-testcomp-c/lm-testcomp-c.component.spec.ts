29e0b0ae6125aaabfbd48224a4d0a1403b3ccc10
Joshua Wiens <joshuaw@easymetrics.com>
2017-05-05 02:18:56 -0500

    test(lazy): Adds dynamic css test

--- src/app/lazy-module/lm-testcomp-c/lm-testcomp-c.component.spec.ts
+++ src/app/lazy-module/lm-testcomp-c/lm-testcomp-c.component.spec.ts
@@ -1,4 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 import { LmTestcompCComponent } from './lm-testcomp-c.component';
 
@@ -21,5 +23,23 @@ describe('Component: LmTestcompCComponent', () => {
 
   it('Should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Should have the .active class if isActive is set true', () => {
+    component.isActive = true;
+    fixture.detectChanges();
+
+    const classes: any = fixture.debugElement.query(By.css('div')).classes;
+    expect(classes.active).toBeTruthy();
+    expect(classes.inactive).toBeFalsy();
+  });
+
+  it('Should have the .inactive class if isActive is set false', () => {
+    component.isActive = false;
+    fixture.detectChanges();
+
+    const classes: any = fixture.debugElement.query(By.css('div')).classes;
+    expect(classes.inactive).toBeTruthy();
+    expect(classes.active).toBeFalsy();
   });
 });
