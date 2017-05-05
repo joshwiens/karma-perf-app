5a5eea5f3272aac8f9871ff79e231e4a787d8a38
Joshua Wiens <joshuaw@easymetrics.com>
2017-05-05 03:49:47 -0500

    test(inline style): Adds dynamic style test

--- src/app/inline-style/is-testcomp-d/is-testcomp-d.component.spec.ts
+++ src/app/inline-style/is-testcomp-d/is-testcomp-d.component.spec.ts
@@ -1,4 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+
+import { customMatchers } from '../../shared/utils/custom-matchers';
 
 import { IsTestcompDComponent } from './is-testcomp-d.component';
 
@@ -7,6 +11,8 @@ describe('Component: IsTestcompDComponent', () => {
   let fixture: ComponentFixture<IsTestcompDComponent>;
 
   beforeEach(async(() => {
+    jasmine.addMatchers(customMatchers);
+
     TestBed.configureTestingModule({
       declarations: [ IsTestcompDComponent ]
     })
@@ -21,5 +27,9 @@ describe('Component: IsTestcompDComponent', () => {
 
   it('Should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Should correctly set the background style', () => {
+      expect(fixture.debugElement.children[0].nativeElement).toHaveCssStyle({ 'background-color': 'red'});
   });
 });
--- src/app/inline-style/is-testcomp-d/is-testcomp-d.component.ts
+++ src/app/inline-style/is-testcomp-d/is-testcomp-d.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class IsTestcompDComponent implements OnInit {
+  public bgColor = 'red';
 
   constructor() { }
 
